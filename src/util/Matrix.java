8055df747bb0d19cdc66789a50e0a4aa5264e75b
Kenneth <kanisiuskenneth@gmail.com>
2017-03-23 10:16:23 +0700

    Add Cell Matrix

---
+++ src/util/Matrix.java
@@ -0,0 +1,26 @@
+
+package util;
+import cell.Cell;
+
+/**
+ * Created by ken on 3/19/17.
+ */
+public class Matrix {
+    Cell[][] Data;
+    int nrow;
+    int ncol;
+
+    public Matrix(int row, int col) {
+        Data = new Cell[][][row][col];
+        nrow = row;
+        ncol = col;
+    }
+
+    public void setData(int i, int j, Cell data) {
+        Data[i][j]=data;
+    }
+
+    public Cell getData(int i,int j) {
+        return Data[i][j];
+    }
+}
