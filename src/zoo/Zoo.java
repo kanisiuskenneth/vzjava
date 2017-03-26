d8a379606b0c1f5781e17c99557d7953a21dbec8
Irene Edria <ireneedriadr@gmail.com>
2017-03-27 03:26:03 +0700

    add zoo, tapi masih belom bisa di build

--- src/zoo/Zoo.java
+++ src/zoo/Zoo.java
@@ -1,10 +1,24 @@
 package zoo;
-
+import util.Matrix;
 /**
  * Created by ken on 3/26/17.
  */
 public class Zoo {
+  public class Zoo {
     private int row;
     private int col;
+    public Matrix data;
 
+    public Zoo(int _row, int _col) {
+      row = _row;
+      col = _col;
+      data = new Matrix(_row,_col);
+    }
+    public int getCol() {
+      return col;
+    }
+    public int getRow() {
+      return row;
+    }
+  }
 }
