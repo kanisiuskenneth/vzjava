28095dda947666c79aa98867a86e35440c141b69
Irene Edria <ireneedriadr@gmail.com>
2017-03-26 22:31:42 +0700

    person

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,7 +1,42 @@
 package person;
 
+import util.Position;
+
 /**
- * Created by ireneedriadr on 3/26/17.
+ * Created by Irene Edria on 3/26/17.
  */
 public class Person {
+  private StringBuffer name;
+  private char symbol;
+  private Position position;
+
+  public Person(StringBuffer alias, Position pos) {
+    name = alias;
+    position = pos;
+    symbol = '9';
+  }
+
+  public Position getPosition() {
+    return position;
+  }
+
+  public void moveUp() {
+    position.row--;
+  }
+
+  public void moveDown() {
+    position.row++;
+  }
+
+  public void moveLeft() {
+    position.col--;
+  }
+
+  public void moveRight() {
+    position.col++;
+  }
+
+  public char render() {
+    return symbol;
+  }
 }
