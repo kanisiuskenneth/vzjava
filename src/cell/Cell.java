0d2220e116d90f274704010ca6927a0d75eeb189
Irene Edria <ireneedriadr@gmail.com>
2017-03-27 02:36:47 +0700

    cell

--- src/cell/Cell.java
+++ src/cell/Cell.java
@@ -3,53 +3,156 @@ package cell;
 /**
  * Created by Irene Edria on 3/23/17.
  */
-
-/**
- * Abstract Class Cell
- * Menciptakan Cell beserta atributnya
- */
 public class Cell {
-    private char symbol;
-    private boolean human_pass;
-    private boolean animal_pass;
+    public char symbol;
+    public boolean human_pass;
+    public boolean animal_pass;
+    public char hab_type;
+    public boolean gate_up;
+    public boolean gate_down;
+    public boolean gate_left;
+    public boolean gate_right;
+    public boolean incage;
 
-    /**
-     * Class constructor.
-     * @param c symbol dari Cell
-     * @param hpass boolean apakah bisa dilewati person atau tidak
-     * @param apass boolean apakah bisa dilewati animal atau tidak
-     */
-    public Cell(char c, boolean hpass, boolean apass) {
-        symbol = c;
-        human_pass = hpass;
-        animal_pass = apass;
+    public Cell(char c) {
+      symbol = c;
+      char type = c;
+
+      switch(type) {
+        case 'a' :
+          human_pass = false;
+          animal_pass = true;
+          hab_type = 'a';
+          gate_up = false;
+          gate_down = false;
+          gate_left = false;
+          gate_right = false;
+          incage = false;
+          break;
+        case 'w' :
+          human_pass = false;
+          animal_pass = true;
+          hab_type = 'w';
+          gate_up = false;
+          gate_down = false;
+          gate_left = false;
+          gate_right = false;
+          incage = false;
+          break;
+        case 'l' :
+          human_pass = false;
+          animal_pass = true;
+          hab_type = 'l';
+          gate_up = false;
+          gate_down = false;
+          gate_left = false;
+          gate_right = false;
+          incage = false;
+          break;
+        case 'e' :
+          human_pass = true;
+          animal_pass = false;
+          hab_type = 'n';
+          gate_up = false;
+          gate_down = false;
+          gate_left = false;
+          gate_right = false;
+          incage = false;
+          break;
+        case 'x' :
+          human_pass = true;
+          animal_pass = false;
+          hab_type = 'n';
+          gate_up = false;
+          gate_down = false;
+          gate_left = false;
+          gate_right = false;
+          incage = false;
+          break;
+        case 'r' :
+          human_pass = true;
+          animal_pass = false;
+          hab_type = 'n';
+          gate_up = false;
+          gate_down = false;
+          gate_left = false;
+          gate_right = false;
+          incage = false;
+          break;
+        case '*' :
+          human_pass = true;
+          animal_pass = false;
+          hab_type = 'n';
+          gate_up = false;
+          gate_down = false;
+          gate_left = false;
+          gate_right = false;
+          incage = false;
+          break;
+        case '.' :
+          human_pass = true;
+          animal_pass = false;
+          hab_type = 'n';
+          gate_up = false;
+          gate_down = false;
+          gate_left = false;
+          gate_right = false;
+          incage = false;
+          break;
+        default :
+          System.out.println("Invalid input");
+      }
     }
 
-    /**
-     * Method untuk mendapatkan simbol Cell
-     * @return simbol dari Cell
-     */
     public char render() {
-        if((this instanceof Habitat) && ((Habitat)this).isInCage()) {
-                return Character.toUpperCase(symbol);
-        } else {
-            return symbol;
-        }
+        return symbol;
     }
 
-    /**
-     * Method untuk mendapatkan boolean human_pass
-     * @return boolean apakah cell bisa dilewati person
-     */
     public boolean isHumanPass() {
         return human_pass;
     }
 
-    /**
-     * Method untuk mendapatkan boolean animal_pass
-     * @return boolean apakah cell bisa dilewati animal
-     */
     public boolean isAnimalPass() {
         return animal_pass;
+    }
+
+    public void openGateUp() {
+      gate_up = true;
+    }
+
+    public void openGateDown() {
+      gate_down = true;
+    }
+
+    public void openGateLeft() {
+      gate_left = true;
+    }
+
+    public void openGateRight() {
+      gate_right = true;
+    }
+
+    public void closeGateUp() {
+      gate_up = false;
+    }
+
+    public void closeGateDown() {
+      gate_down = false;
+    }
+
+    public void closeGateLeft() {
+      gate_left = false;
+    }
+
+    public void closeGateRight() {
+      gate_right = false;
+    }
+
+    public char GetHabitatType() {
+      return hab_type;
+    }
+
+    public boolean IsInCage() {
+      return incage;
     }
 }
