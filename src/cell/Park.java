59c01220ac0609d845746093efb5e041c75c90e1
Irene Edria <ireneedriadr@gmail.com>
2017-03-26 21:50:34 +0700

    ngebenrin cell

--- src/cell/Park.java
+++ src/cell/Park.java
@@ -1,7 +1,7 @@
 package cell;
 
 /**
- * Created by ireneedriadr on 3/26/17.
+ * Created by Irene Edria on 3/26/17.
  */
 public class Park extends Facility {
   public Park() {
---
+++ src/person/Person.java
@@ -0,0 +1,7 @@
+package person;
+
+/**
+ * Created by ireneedriadr on 3/26/17.
+ */
+public class Person {
+}
