382dc5a039b40351366ff73ff529be3314547e60
reeechart <ferdinandusrichard@yahoo.co.id>
2017-03-28 19:40:21 +0700

    Javadoc Animal (doang)

--- src/animal/Animal.java
+++ src/animal/Animal.java
@@ -1,3 +1,5 @@
+// File : Animal.java
+
 package animal;
 
 import java.util.Set;
@@ -6,7 +8,12 @@ import util.Position;
 import util.Renderable;
 
 /**
- * Created by Richard on 26-Mar-17.
+ * Created by Ferdinandus Richard on 26-Mar-17.
+ */
+
+/**
+ * Abstract class Animal yang mengimplementasikan kelas interface renderable
+ * Menciptakan Animal beserta atributnya
  */
 public abstract class Animal implements Renderable {
     private StringBuffer name;
@@ -25,6 +32,14 @@ public abstract class Animal implements Renderable {
     protected String genus;
     protected String spesies;
 
+    /**
+     * Class constructor.
+     * @param animal_name nama dari Animal yang akan dibuat
+     * @param animal_weight berat dari Animal yang akan dibuat
+     * @param identifier ID dari Animal yang diasosiasikan dengan tipe Animal yang dibuat
+     * @param pos posisi Animal yang akan dibentuk
+     * @param sym simbol Animal yang akan dibentuk
+     */
     public Animal(StringBuffer animal_name, int animal_weight, int identifier, Position pos, char sym) {
         name = animal_name;
         weight = animal_weight;
@@ -33,49 +48,94 @@ public abstract class Animal implements Renderable {
         symbol = sym;
     }
 
+    /**
+     * Method untuk mendapatkan weight dari Animal
+     * @return weight dari Animal
+     */
     public int getWeight() {
         return weight;
     }
 
+    /**
+     * Method untuk mendapatkan ID (identifier) dari Animal
+     * @return ID dari Animal
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Method Animal untuk bergerak ke atas dalam map
+     */
     public void moveUp() {
         position.row--;
     }
 
+    /**
+     * Method dari Animal untuk bergerak ke bawah dalam map
+     */
     public void moveDown() {
         position.row++;
     }
 
+    /**
+     * Method dari Animal untuk bergerak ke kiri dalam map
+     */
     public void moveLeft() {
         position.col--;
     }
 
+    /**
+     * Method dari Animal untuk bergerak ke kanan dalam map
+     */
     public void moveRight() {
         position.col++;
     }
 
+    /**
+     * Method render mengimplementasikan kelas Renderable
+     * Method ini berfungsi untuk mendapatkan simbol dari Animal
+     * @return char yang menandakan Animal tersebut
+     */
     public char render() {
         return symbol;
     }
 
+    /**
+     * Method dari Animal untuk mendapatkan Position dari Animal
+     * @return Position dari Animal yang sudah dibentuk
+     */
     public Position getPosition() {
         return position;
     }
 
+    /**
+     * Method mengambil set yang diambil adalah set yang berisi Animal yang tidak cocok dengan dirinya
+     * @return Set of Incompatible
+     */
     public Set<Integer> getIncompatible() {
         return incompatible;
     }
 
+    /**
+     * Method mengambil list dari habitat yang dapat ditinggali Animal
+     * @return List of Habitat
+     */
     public List<Character> getHabitats() {
         return habitats;
     }
 
+    /**
+     * Method dari Animal untuk mendapatkan nama dari Animal
+     * @return String yang merupakan nama dari Animal
+     */
     public StringBuffer getName() {
         return name;
     }
 
-    public abstract String  interact();
+    /**
+     * Method abstrak yang menghasilkan suara dari Animal
+     * @return String suara dari Animal
+     */
+    public abstract String interact();
 }
