4970eb9b4d3bfbf55114bcbccf81d2499f27edc2
reeechart <ferdinandusrichard@yahoo.co.id>
2017-03-28 07:49:47 +0700

    tambah cage v2, edit zoo v2

--- src/cage/Cage.java
+++ src/cage/Cage.java
@@ -1,15 +1,14 @@
 package cage;
 
-import animal.*;
-import animal.Chimpanzee;
+import animal.Animal;
 import cell.Cell;
-import cell.Habitat;
+import util.Position;
 
 import java.util.HashSet;
 import java.util.List;
 
 /**
- * Created by kennethhalim on 3/27/17.
+ * Created by Richard on 28-Mar-17.
  */
 public class Cage {
     public HashSet<Cell> cells = new HashSet<Cell>();
@@ -23,93 +22,28 @@ public class Cage {
         return type;
     }
     private boolean isAnimalCompatible(int id) {
-        boolean f = true;
-        for(Animal it : animals) {
-            if(it.getIncompatible().contains(id)) {
-                f = false;
+        boolean compatibility = true;
+        for (Animal iterator : animals) {
+            if (iterator.getIncompatible().contains(id)) {
+                compatibility = false;
             }
         }
-        return f;
+        return compatibility;
     }
     private boolean isHabitatCompatible(List<Character> hab) {
-        boolean f = false;
+        boolean compatibility = true;
         for (Cell it : cells) {
-            for(Character jt : hab) {
-                if (((Habitat) it).getHabitatType() == jt) {
-                    f = true;
+            for (Character jt : hab) {
+                if ((Cell) it).GetHabitatType() == jt) {
+                    compatibility = false;
                 }
             }
         }
-        return f;
+        return compatibility;
     }
-    public void addAnimal(int id, StringBuffer name, int r, int c) {
-        Animal buffer = null;
-        switch (id) {
-            case 1:
-                buffer = new Deer(name,r,c);
-                break;
-            case 2:
-                buffer = new Chimpanzee(name,r,c);
-                break;
-            case 3:
-                buffer = new Bear(name,r,c);
-                break;
-            case 4:
-                buffer = new Elephant(name,r,c);
-                break;
-            case 5:
-                buffer = new Zebra(name,r,c);
-                break;
-            case 6:
-                buffer = new Tiger(name,r,c);
-                break;
-            case 7:
-                buffer = new Lion(name,r,c);
-                break;
-            case 8:
-                buffer = new Giraffe(name,r,c);
-                break;
-            case 9:
-                buffer = new Komodo(name,r,c);
-                break;
-            case 10:
-                buffer = new ElectricEel(name,r,c);
-                break;
-            case 11:
-                buffer = new Shark(name,r,c);
-                break;
-            case 12:
-                buffer = new Seal(name,r,c);
-                break;
-            case 13:
-                buffer = new Stingray(name,r,c);
-                break;
-            case 14:
-                buffer = new Jellyfish(name,r,c);
-                break;
-            case 15:
-                buffer = new Octopus(name,r,c);
-                break;
-            case 16:
-                buffer = new Owl(name,r,c);
-                break;
-            case 17:
-                buffer = new Pelican(name,r,c);
-                break;
-            case 18:
-                buffer = new Cockatoo(name,r,c);
-                break;
-            case 19:
-                buffer = new Parrot(name,r,c);
-                break;
-            case 20:
-                buffer = new Eagle(name,r,c);
-                break;
-            case 21:
-                buffer = new Turtle(name,r,c);
-                break;
-        }
-        if(isAnimalCompatible(id) && isHabitatCompatible(buffer.getHabitats())) {
+    public void AddAnimal(int id, StringBuffer name, int row, int col) {
+        Animal buffer = new Animal(name, id, Position.makePos(row,col));
+        if (isAnimalCompatible(id) && isHabitatCompatible(buffer.getHabitats())) {
             animals.add(buffer);
         }
     }
